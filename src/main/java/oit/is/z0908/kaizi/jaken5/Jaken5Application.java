8f1b256a206566bd87501d790fa08fde4ab95b27
Shoya1837 <e1b18009@oit.ac.jp>
2020-10-27 15:21:11 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0908/kaizi/jaken5/Jaken5Application.java
@@ -0,0 +1,13 @@
+package oit.is.z0908.kaizi.jaken5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaken5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaken5Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0908/kaizi/jaken5/Jaken5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0908.kaizi.jaken5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaken5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
